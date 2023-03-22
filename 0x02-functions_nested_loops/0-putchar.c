2328ec5a8d20a78777ccbb3542f766e243fb3331
aminelubombashi <aminedari2002@gmail.com>
2023-03-21 17:24:52 -0700

    hello c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
-i#include "main.h"
+#include "main.h"
 /**
  * main - print _putchar
  * is written by mbah
  * Return: 0
  */
-
 
 int main(void)
 {
@@ -18,3 +17,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
+}
